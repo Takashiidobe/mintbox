493ce8096e360b6b21f8ba969d5a72f3fea3e553
Takashiidobe <idobetakashi@gmail.com>
2025-11-04 19:11:48 -0500

    remove code that prevented fwrite from logging to console

--- src/libc/stdio/fwrite.c
+++ src/libc/stdio/fwrite.c
@@ -6,12 +6,6 @@ size_t fwrite(const void *ptr, size_t size, size_t nmemb, FILE *stream) {
     return 0;
 
   size_t total_bytes = size * nmemb;
-
-  if ((stream->flags & FILE_FLAG_STATIC) &&
-      (stream->handle == 1 || stream->handle == 2)) {
-    return total_bytes / size;
-  }
-
   long ret = Fwrite((short)stream->handle, (long)total_bytes, ptr);
   if (ret <= 0)
     return 0;
